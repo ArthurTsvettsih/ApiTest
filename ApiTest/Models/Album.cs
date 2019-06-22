f159b823849bb65b4a40f340f7a188128fd8a9ae
Arthur Tsvettsih <arthur.tsvettsih@gmail.com>
2019-06-22 11:51:21 +0100

    Added placeholder interfaces and Models.csproj

---
+++ ApiTest/Models/Album.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Models
+{
+    public class Album
+    {
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<Photo> Photos { get; set; }
+    }
+}
---
+++ ApiTest/Services/IAlbumsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Models;
+
+namespace Services
+{
+    public interface IAlbumsService
+    {
+        List<Album> GetAlbums();
+        List<Album> GetAlbumsByUserId();
+    }
+}
